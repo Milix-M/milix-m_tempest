d74aa126018f60236425b1aa7bed1228051194d1
Milix-M <tujikou12dh@gmail.com>
2024-02-29 08:35:58 +0900

    add daisyui settings

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  daisyui: {
+    themes: ["dark", "light"]
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
