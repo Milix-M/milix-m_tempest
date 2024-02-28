310ad8557c22ae2c408bf4728692339fc7e3afe5
Milix-M <tujikou12dh@gmail.com>
2024-02-29 08:36:25 +0900

    fix theme change system

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout ({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute='class'>{children}</ThemeProvider>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   )
