cf25ac8bd18e40639ddf09f545252ce78112d923
Milix-M <tujikou12dh@gmail.com>
2024-03-01 01:27:24 +0900

    add footer

---
+++ web/src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import { FaXTwitter } from 'react-icons/fa6'
+import { SiZenn } from 'react-icons/si'
+
+const Footer = () => {
+  return (
+    <footer className='footer items-center p-4 bg-neutral text-neutral-content'>
+      <aside className='items-center grid-flow-col'>
+        <p>© 2023 {process.env.NEXT_PUBLIC_REALNAME}. All rights reserved.</p>
+      </aside>
+      <nav className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
+        {process.env.NEXT_PUBLIC_ZENN && (
+          <a href={process.env.NEXT_PUBLIC_ZENN as string}>
+            <SiZenn className='w-6 h-6' />
+          </a>
+        )}
+
+        {process.env.NEXT_PUBLIC_TWITTER && (
+          <a href={process.env.NEXT_PUBLIC_TWITTER as string}>
+            <FaXTwitter className='w-6 h-6' />
+          </a>
+        )}
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
