ef46f80fd2a764a085b324c013344e37045ba46e
Milix-M <tujikou12dh@gmail.com>
2024-03-01 01:43:44 +0900

    edit padding

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home () {
   return (
     <div>
       <Header />
-      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <main className='flex min-h-screen flex-col items-center justify-between p-40'>
         <Index />
       </main>
       <Footer />
