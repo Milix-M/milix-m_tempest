1704aea3379c1db566a420455fbbbd3e4e88f118
Milix-M <tujikou12dh@gmail.com>
2024-03-01 01:34:45 +0900

    change color && fixed navbar

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -20,83 +20,86 @@ const Header = () => {
   }
 
   return (
-    <div className='navbar bg-base-100'>
-      <div className='navbar-start'>
-        <div className='dropdown'>
-          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h8m-8 6h16'
-              />
-            </svg>
+    <div className='relative'>
+      <div className='fixed top-0 left-0 right-0 z-[9999999999]'>
+        <div className='navbar bg-base-300'>
+          <div className='navbar-start'>
+            <div className='dropdown'>
+              <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-5 w-5'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h8m-8 6h16'
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box'
+              >
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>Profile</a>
+                </li>
+                <li>
+                  <a>Works</a>
+                </li>
+                <li>
+                  <a>Contact</a>
+                </li>
+                <li>
+                  <a>Zenn</a>
+                </li>
+              </ul>
+            </div>
+            <a className='btn btn-ghost text-xl'>
+              {process.env.NEXT_PUBLIC_REALNAME}
+            </a>
           </div>
-          <ul
-            tabIndex={0}
-            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box'
-          >
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Profile</a>
-            </li>
-            <li>
-              <a>Works</a>
-            </li>
-            <li>
-              <a>Contact</a>
-            </li>
-            <li>
-              <a>Zenn</a>
-            </li>
-          </ul>
+          <div className='navbar-center hidden lg:flex'>
+            <ul className='menu menu-horizontal px-1'>
+              <li>
+                <a className='font-bold'>Home</a>
+              </li>
+              <li>
+                <a className='font-bold'>Profile</a>
+              </li>
+              <li>
+                <a className='font-bold'>Works</a>
+              </li>
+              <li>
+                <a className='font-bold'>Contact</a>
+              </li>
+              <li>
+                <a className='font-bold'>Zenn</a>
+              </li>
+            </ul>
+          </div>
+          <div className='navbar-end'>
+            {/* sun icon */}
+            {theme === 'dark' && (
+              <button className='btn btn-circle' onClick={themeSwitcher}>
+                <FaRegSun className='swap-on w-5 h-5' />
+              </button>
+            )}
+            {/* moon icon */}
+            {theme === 'light' && (
+              <button className='btn btn-circle' onClick={themeSwitcher}>
+                <FaRegMoon className='swap-off w-5 h-5' />
+              </button>
+            )}
+          </div>
         </div>
-        <a className='btn btn-ghost text-xl'>
-          {process.env.NEXT_PUBLIC_REALNAME}
-        </a>
-      </div>
-      <div className='navbar-center hidden lg:flex'>
-        <ul className='menu menu-horizontal px-1'>
-          <li>
-            <a className='font-bold'>Home</a>
-          </li>
-          <li>
-            <a className='font-bold'>Profile</a>
-          </li>
-          <li>
-            <a className='font-bold'>Works</a>
-          </li>
-          <li>
-            <a className='font-bold'>Contact</a>
-          </li>
-          <li>
-            <a className='font-bold'>Zenn</a>
-          </li>
-        </ul>
-      </div>
-      <div className='navbar-end'>
-        {/* sun icon */}
-        {theme === 'dark' && (
-          <button className='btn btn-circle' onClick={themeSwitcher}>
-            <FaRegSun className='swap-on w-5 h-5' />
-          </button>
-        )}
-
-        {/* moon icon */}
-        {theme === 'light' && (
-          <button className='btn btn-circle' onClick={themeSwitcher}>
-            <FaRegMoon className='swap-off w-5 h-5' />
-          </button>
-        )}
       </div>
     </div>
   )
