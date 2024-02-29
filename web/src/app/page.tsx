501f7c496ddafeedb3c0af4bee47e6880128f08d
Milix-M <tujikou12dh@gmail.com>
2024-03-01 01:01:16 +0900

    add component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import Header from '@/components/Header/Header'
+import Index from '@/components/Home/Index'
 
 export default function Home () {
   return (
     <div>
       <Header />
       <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <Index />
       </main>
     </div>
   )
