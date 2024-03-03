34193f10f559c27ccc29fdaec5edfa4494a2c927
Milix-M <tujikou12dh@gmail.com>
2024-03-03 23:38:49 +0900

    add padding

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import Index from '@/components/Home/Index'
+import Profile from '@/components/Home/Profile'
 
 export default function Home () {
   return (
-    <div>
-      <Index />
+    <div
+      className='mt-[68px] h-dvh flex items-center justify-center w-full p-2'
+      // style={{
+      //   backgroundImage: `url("/bg_img.jpg")`,
+      //   backgroundSize: 'cover',
+      //   borderImageRepeat: 'stretch'
+      // }}
+    >
+      <Profile />
     </div>
   )
 }
