ed9479bc5528c756a8fd6ab56e55e4e7ba2c40c3
Milix-M <tujikou12dh@gmail.com>
2024-03-04 04:48:01 +0900

    add Skills component

---
+++ web/src/components/Skills/Skills.tsx
@@ -0,0 +1,23 @@
+type SkillsProps = {
+  title: string
+  description: string
+  icon: React.ReactNode
+}
+
+const Skills: React.FC<SkillsProps> = ({ title, description, icon }) => {
+  return (
+    <div className='max-w-sm bg-neutral p-3 rounded'>
+      <div className='flex'>
+        <div className='mr-3 rounded-md aspect-square w-full max-h-14 flex justify-center items-center'>
+          {icon}
+        </div>
+        <div>
+          <h2 className='text-xl font-bold'>{title}</h2>
+          <p className='break-words'>{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
