b8b753d1f759fea35b6ee2dabea247f06a128293
Milix-M <tujikou12dh@gmail.com>
2024-03-05 00:51:02 +0900

    fix design

--- web/src/components/Skills/Skills.tsx
+++ web/src/components/Skills/Skills.tsx
@@ -7,13 +7,13 @@ type SkillsProps = {
 const Skills: React.FC<SkillsProps> = ({ title, description, icon }) => {
   return (
     <div className='max-w-md bg-neutral p-3 rounded'>
-      <div className='flex'>
-        <div className='mr-3 rounded-md aspect-square w-full max-h-14 flex justify-center items-center'>
-          {icon}
+      <div className='flex h-full w-full'>
+        <div className='rounded-md mr-4 items-center'>
+          <div className='justify-center items-center flex p-3 rounded'>{icon}</div>
         </div>
         <div>
-          <h2 className='text-xl font-bold'>{title}</h2>
-          <p className='break-words'>{description}</p>
+          <h2 className='text-xl font-bold my-1'>{title}</h2>
+          <p className='break-words text-slate-400'>{description}</p>
         </div>
       </div>
     </div>
