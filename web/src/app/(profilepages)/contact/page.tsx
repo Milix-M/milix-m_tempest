0428592d131d7c5841bda6227e8d2a580879c80c
Milix-M <tujikou12dh@gmail.com>
2024-03-07 23:16:31 +0900

    add validation

--- web/src/app/(profilepages)/contact/page.tsx
+++ web/src/app/(profilepages)/contact/page.tsx
@@ -1,51 +1,89 @@
 'use client'
 
-// todo React hook form 導入
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup
+  .object({
+    name: yup.string().required('名前を入力してください'),
+    email: yup
+      .string()
+      .email('有効な形式ではありません')
+      .required('メールアドレスを入力してください'),
+    message: yup.string().required('お問い合わせ内容を入力してください')
+  })
+  .required()
 
 export default function Home () {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+  const onSubmit = () => console.log()
+
   return (
     <div>
       <h1 className='font-bold text-3xl sm:mb-10'>Contact</h1>
-      <div>
-        <div className='grid sm:grid-cols-2 gap-5 grid-cols-1'>
-          <div className=''>
-            <label className='form-control w-full'>
-              <div className='label'>
-                <span className='label-text'>名前</span>
-                <span className='label-text-alt'>必須</span>
-              </div>
-              <input
-                type='text'
-                placeholder='Your Name'
-                className='input input-bordered w-full'
-              />
-            </label>
+      <form action='' onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <div className='grid sm:grid-cols-2 gap-5 grid-cols-1'>
+            <div className=''>
+              <label className='form-control w-full'>
+                <div className='label'>
+                  <span className='label-text'>名前</span>
+                  <span className='label-text-alt'>必須</span>
+                </div>
+                <input
+                  type='text'
+                  placeholder='Your Name'
+                  className='input input-bordered w-full'
+                  {...register('name')}
+                />
+              </label>
+              {errors.name?.message && (
+                <span className='text-sm'>{errors.name?.message}</span>
+              )}
+            </div>
+            <div className=''>
+              <label className='form-control w-full'>
+                <div className='label'>
+                  <span className='label-text'>メールアドレス</span>
+                  <span className='label-text-alt'>必須</span>
+                </div>
+                <input
+                  type='email'
+                  placeholder='Your Email Address'
+                  className='input input-bordered w-full'
+                  {...register('email')}
+                />
+              </label>
+              {errors.email?.message && (
+                <span className='text-sm'>{errors.email?.message}</span>
+              )}
+            </div>
           </div>
-          <div className=''>
-            <label className='form-control w-full'>
-              <div className='label'>
-                <span className='label-text'>メールアドレス</span>
-                <span className='label-text-alt'>必須</span>
-              </div>
-              <input
-                type='email'
-                placeholder='Your Email Address'
-                className='input input-bordered w-full'
-              />
-            </label>
+          <label className='form-control w-full mt-4'>
+            <div className='label'>
+              <span className='label-text'>お問い合わせ内容</span>
+              <span className='label-text-alt'>必須</span>
+            </div>
+            <textarea
+              className='textarea textarea-bordered h-32'
+              {...register('message')}
+            ></textarea>
+          </label>
+          {errors.message?.message && (
+            <span className='text-sm'>{errors.message?.message}</span>
+          )}
+          <div className='flex justify-center mt-4'>
+            <button className='btn'>お問い合わせ送信</button>
           </div>
         </div>
-        <label className='form-control w-full mt-4'>
-          <div className='label'>
-            <span className='label-text'>お問い合わせ内容</span>
-            <span className='label-text-alt'>必須</span>
-          </div>
-          <textarea className='textarea textarea-bordered h-32'></textarea>
-        </label>
-        <div className='flex justify-center mt-4'>
-          <button className='btn'>お問い合わせ送信</button>
-        </div>
-      </div>
+      </form>
     </div>
   )
 }
