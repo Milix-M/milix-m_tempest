74440cd0e984d8f30ec5425a09735ef30d1d0c6b
Milix-M <tujikou12dh@gmail.com>
2024-03-10 23:59:59 +0900

    edit margin

--- web/src/app/(profilepages)/works/page.tsx
+++ web/src/app/(profilepages)/works/page.tsx
@@ -3,7 +3,7 @@
 export default function Home () {
   return (
     <div>
-      <h1 className='font-bold text-3xl sm:mb-10'>Works</h1>
+      <h1 className='font-bold text-3xl mb-7'>Works</h1>
       <span>・現在公開しているプロジェクトはありません</span>
     </div>
   )
