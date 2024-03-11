bb905b5ec7dd0d0c3348ccaa0d370399676752ea
Milix-M <tujikou12dh@gmail.com>
2024-03-11 23:11:25 +0900

    add 32theme support

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -2,7 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   daisyui: {
-    themes: ["dark", "light"]
+    themes: [
+      "light", "dark", "cupcake", "bumblebee",
+      "emerald", "corporate", "synthwave", "retro",
+      "cyberpunk", "valentine", "halloween", "garden",
+      "forest", "aqua", "lofi", "pastel",
+      "fantasy", "wireframe", "black", "luxury",
+      "dracula", "cmyk", "autumn", "business",
+      "acid", "lemonade", "night", "coffee",
+      "winter", "dim", "nord", "sunset",
+    ]
   },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
