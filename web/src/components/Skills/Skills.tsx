74915ccdfc1bfcbe934686566612f03d3f79ad51
Milix-M <tujikou12dh@gmail.com>
2024-03-14 23:30:42 +0900

    update style

--- web/src/components/Skills/Skills.tsx
+++ web/src/components/Skills/Skills.tsx
@@ -6,14 +6,14 @@ type SkillsProps = {
 
 const Skills: React.FC<SkillsProps> = ({ title, description, icon }) => {
   return (
-    <div className='bg-neutral p-3 rounded'>
+    <div className='bg-base-300 p-3 rounded'>
       <div className='flex h-full w-full'>
         <div className='rounded-md mr-4 items-center'>
           <div className='justify-center items-center flex p-3 rounded'>{icon}</div>
         </div>
         <div>
           <h2 className='text-xl font-bold my-1'>{title}</h2>
-          <p className='break-words text-slate-400'>{description}</p>
+          <p className='break-words'>{description}</p>
         </div>
       </div>
     </div>
