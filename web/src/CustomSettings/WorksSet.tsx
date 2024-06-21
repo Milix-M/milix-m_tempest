13dd53b5153329ca9d52a8d98a8f107cec326287
Milix-M <tujikou12dh@gmail.com>
2024-06-21 13:00:11 +0900

    add work

--- web/src/CustomSettings/WorksSet.tsx
+++ web/src/CustomSettings/WorksSet.tsx
@@ -5,7 +5,7 @@ export default function WorksSet() {
     <>
       <Works title="Passgen" description="React+MUI+TSで開発したChrome拡張機能です。数クリックで簡単に安全なパスワードが生成できます。" link="https://chromewebstore.google.com/detail/passgen-a-simple-password/jibdlcehfeapakbemeombfeophiahmcg" />
 
-      <Works title="開発中..." description="開発中..." link="https://example.com/"/>
+      <Works title="Authenticator" description="React+daisyUI(tailwind)+TSで開発したChrome拡張機能です。ブラウザ上で簡単に二段階認証のコードを生成することができます。" link="https://chromewebstore.google.com/detail/authenticator/biblcaincgpfkamhjigfmdpdmbdeojcp"/>
     </>
   );
 }
