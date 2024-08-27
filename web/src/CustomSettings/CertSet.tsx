068bb669efa9a53cb7122bea0dadc13eb4480386
Milix-M <tujikou12dh@gmail.com>
2024-08-27 11:34:02 +0900

    add Cert

--- web/src/CustomSettings/CertSet.tsx
+++ web/src/CustomSettings/CertSet.tsx
@@ -4,13 +4,22 @@ import { FaJava } from "react-icons/fa";
 export default function CertSet() {
   return (
     <>
-      <Certification
-        company="Oracle Japan"
-        title="Oracle Certified Java Programmer, Silver SE 11"
-        getDate="2024/05/26"
-        description="Javaアプリケーション開発に必要とされる基本的なプログラミング知識を有し、上級者の指導のもとで開発作業を行うことができる開発初心者向け資格。"
-        icon={<FaJava className="w-9 h-9" />}
-      />
+      <div className="space-y-4">
+        <Certification
+          company="Oracle Japan"
+          title="Oracle Certified Java Programmer, Silver SE 11"
+          getDate="2024/05/26"
+          description="Javaアプリケーション開発に必要とされる基本的なプログラミング知識を有し、上級者の指導のもとで開発作業を行うことができる開発初心者向け資格。"
+          icon={<FaJava className="w-9 h-9" />}
+        />
+        <Certification
+          company="Oracle Japan"
+          title="Oracle Certified Java Programmer, Gold SE 11"
+          getDate="2024/05/26"
+          description="設計者の意図を正しく理解して独力で機能実装が行える中上級者向け資格。"
+          icon={<FaJava className="w-9 h-9" />}
+        />
+      </div>
     </>
   );
 }
