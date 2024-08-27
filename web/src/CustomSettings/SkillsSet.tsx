f747e5da1290e4a667aedd082b9f2b16a7ec576a
Milix-M <tujikou12dh@gmail.com>
2024-08-27 11:41:44 +0900

    add skils

--- web/src/CustomSettings/SkillsSet.tsx
+++ web/src/CustomSettings/SkillsSet.tsx
@@ -1,12 +1,12 @@
-import { FaDocker } from "react-icons/fa"; 
-import { FaJava } from "react-icons/fa"; 
-import { SiNextdotjs } from 'react-icons/si'
+import { FaDocker, FaLinux } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { SiKubernetes, SiNextdotjs } from 'react-icons/si'
 import { FaReact } from 'react-icons/fa'
 import Skills from '@/components/Skills/Skills'
 import { FaPython } from 'react-icons/fa'
 import { SiTypescript } from 'react-icons/si'
 
-export default function SkillsSet () {
+export default function SkillsSet() {
   return (
     <>
       <Skills
@@ -16,28 +16,38 @@ export default function SkillsSet () {
       />
       <Skills
         title='TypeScript'
-        description='フロントエンドの開発でこの言語を使用しています。型がかけて便利なので使用しています。'
+        description='フロントエンドの開発でこの言語を使用しています。JSと比べ型がかけて便利です。'
         icon={<SiTypescript className='w-9 h-9' />}
       />
       <Skills
         title='React'
-        description='TypeScriptと合わせて使用しています。最近はこれがないとフロントエンドの開発はできないかも。'
+        description='TypeScriptと合わせて使用しています。'
         icon={<FaReact className='w-9 h-9' />}
       />
       <Skills
         title='Next.js'
-        description='このポートフォリオの開発、SNSアプリの開発で使用しました。Next.jsは最強です。'
+        description='このポートフォリオの開発、SNSアプリの開発で使用しました。'
         icon={<SiNextdotjs className='w-9 h-9' />}
       />
       <Skills
         title='Java'
-        description='仕事で使っている言語です。'
+        description='仕事で使っている言語です。Springフレームワークをメインで使用しています。'
         icon={<FaJava className='w-9 h-9' />}
       />
       <Skills
         title='Docker'
-        description='Docker最強!Docker最強!'
+        description='個人開発から仕事まで幅広く使用しています。'
         icon={<FaDocker className='w-9 h-9' />}
+      />
+      <Skills
+        title='Kubernetes'
+        description='仕事で使用しています。'
+        icon={<SiKubernetes className='w-9 h-9' />}
+      />
+      <Skills
+        title='Linux'
+        description='仕事、趣味で使用しています。'
+        icon={<FaLinux className='w-9 h-9' />}
       />
     </>
   )
