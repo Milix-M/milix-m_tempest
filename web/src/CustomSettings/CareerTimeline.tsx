23ab94d8f85329c64226000e2037ca160d0cfe7d
Milix-M <tujikou12dh@gmail.com>
2025-04-04 00:40:13 +0900

    update career

--- web/src/CustomSettings/CareerTimeline.tsx
+++ web/src/CustomSettings/CareerTimeline.tsx
@@ -19,8 +19,7 @@ const CareerTimeline = () => {
           </div>
           <div className='timeline-start md:text-end mb-10'>
             <time className='font-mono italic'>2021/04</time>
-            <div className='text-lg font-black'>S高等学校 入学</div>
-            普通科に入学、通信制の高校で、一般的な勉強をしました。
+            <div className='text-lg font-black'>S高等学校 普通科 入学</div>
           </div>
           <hr />
         </li>
@@ -43,30 +42,6 @@ const CareerTimeline = () => {
           <div className='timeline-end mb-10'>
             <time className='font-mono italic'>2024/03</time>
             <div className='text-lg font-black'>S高等学校 卒業</div>
-            色々な事を学びました。
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div className='timeline-middle'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              className='h-5 w-5'
-            >
-              <path
-                fillRule='evenodd'
-                d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </div>
-          <div className='timeline-start md:text-end mb-10'>
-            <time className='font-mono italic'>2024/04</time>
-            <div className='text-lg font-black'>名古屋市 IT系中小企業</div>
-            バックエンド・インフラ構築業務に従事。
           </div>
           <hr />
         </li>
@@ -89,7 +64,7 @@ const CareerTimeline = () => {
           <div className='timeline-end mb-10'>
             <time className='font-mono italic'>Now</time>
             <div className='text-lg font-black'>現在</div>
-            Java+Springのバックエンド開発や今ではKubernetesやOpenStack,Linuxを使用したプライベートクラウドの検討、構築業務をしています。
+            名古屋市に本社を置くIT系中小企業にて、Java+Springのマイクロサービス開発・Kubernetes,OpenStack,Linuxを使用したクラウドの検討、構築業務・Pythonを使用したAI学習基盤システムの開発をしています。
           </div>
           {/* <hr /> */}
         </li>
